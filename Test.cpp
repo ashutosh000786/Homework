75e8a2e2660732281e87590754471f515bcee932
Ashutosh <ashutoshmishra000786@gmail.com>
2023-09-05 21:23:47 +0530

    A New file is being added

---
+++ Test.cpp
@@ -0,0 +1,23 @@
+// C++ program to display "Hello World"
+ 
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+ 
+// Main() function: where the execution of
+// program begins
+int main()
+{
+    // Prints hello world
+    cout << "Hello World" << endl;
+    cout << 2<< endl;
+    cout << "2"<< endl;
+    cout << '2'<< endl;
+    int a;
+    cout << "Enter a number : ";
+    cin >> a;
+    cout << "Print A : "<<a;
+    cout << sizeof(a);
+    
+    return 0;
+}
